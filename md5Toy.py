83421293f65b200e369529a8e41ec3442de6400f
libbie1996 <libbie1996@gmail.com>
2020-02-12 06:41:26 -0500

    md5Toy

--- md5Toy.py
+++ md5Toy.py
@@ -4,6 +4,31 @@ from windowBreak import windowBreak
 import random
 import hashlib
 
+                                                                                                                          
+#	                                    dddddddd                                                                              
+#	                                    d::::::d555555555555555555TTTTTTTTTTTTTTTTTTTTTTT                                     
+#	                                    d::::::d5::::::::::::::::5T:::::::::::::::::::::T                                     
+#	                                    d::::::d5::::::::::::::::5T:::::::::::::::::::::T                                     
+#	                                    d:::::d 5:::::555555555555T:::::TT:::::::TT:::::T                                     
+#	   mmmmmmm    mmmmmmm       ddddddddd:::::d 5:::::5           TTTTTT  T:::::T  TTTTTTooooooooooo yyyyyyy           yyyyyyy
+#	 mm:::::::m  m:::::::mm   dd::::::::::::::d 5:::::5                   T:::::T      oo:::::::::::ooy:::::y         y:::::y 
+#	m::::::::::mm::::::::::m d::::::::::::::::d 5:::::5555555555          T:::::T     o:::::::::::::::oy:::::y       y:::::y  
+#	m::::::::::::::::::::::md:::::::ddddd:::::d 5:::::::::::::::5         T:::::T     o:::::ooooo:::::o y:::::y     y:::::y   
+#	m:::::mmm::::::mmm:::::md::::::d    d:::::d 555555555555:::::5        T:::::T     o::::o     o::::o  y:::::y   y:::::y    
+#	m::::m   m::::m   m::::md:::::d     d:::::d             5:::::5       T:::::T     o::::o     o::::o   y:::::y y:::::y     
+#	m::::m   m::::m   m::::md:::::d     d:::::d             5:::::5       T:::::T     o::::o     o::::o    y:::::y:::::y      
+#	m::::m   m::::m   m::::md:::::d     d:::::d 5555555     5:::::5       T:::::T     o::::o     o::::o     y:::::::::y       
+#	m::::m   m::::m   m::::md::::::ddddd::::::dd5::::::55555::::::5     TT:::::::TT   o:::::ooooo:::::o      y:::::::y        
+#	m::::m   m::::m   m::::m d:::::::::::::::::d 55:::::::::::::55      T:::::::::T   o:::::::::::::::o       y:::::y         
+#	m::::m   m::::m   m::::m  d:::::::::ddd::::d   55:::::::::55        T:::::::::T    oo:::::::::::oo       y:::::y          
+#	mmmmmm   mmmmmm   mmmmmm   ddddddddd   ddddd     555555555          TTTTTTTTTTT      ooooooooooo        y:::::y           
+#	                                                                                                       y:::::y            
+#	                                                                                                      y:::::y             
+#	                                                                                                     y:::::y              
+#	                                                                                                    y:::::y               
+#	                                                                                                   yyyyyyy                
+
+
 #	Copyright (C) 2020 Jordon Mitchell
 #		This file is part of md5Toy.
 
